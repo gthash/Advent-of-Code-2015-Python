fb7f069072e9fc2fdfec420657802f2b61f8ea62
gthash <74492250+gthash@users.noreply.github.com>
2023-02-28 11:37:44 -0300

    Create part-one.py

---
+++ day1/part-one.py
@@ -0,0 +1,12 @@
+def main():
+    count = 0
+    with open("input.txt") as file:
+        line = file.readline().strip()
+        for char in line:
+            count += 1 if char == "(" else -1
+    print(count)
+
+if __name__ == "__main__":
+    main()
+
+        
