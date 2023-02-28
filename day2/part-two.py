d840ac0a2aeea4d16dffe6f05f96ee2514376025
gthash <74492250+gthash@users.noreply.github.com>
2023-02-28 12:25:01 -0300

    Add files via upload

---
+++ day2/part-two.py
@@ -0,0 +1,14 @@
+import functools
+
+def main():
+    with open("input.txt") as file:
+        count = 0
+        for line in file:
+            numArray = [int(num) for num in line.split("x")]
+            numArray.sort()
+            count += functools.reduce(lambda a, b: a * b, numArray)
+            count += 2 * (numArray[0] + numArray[1])
+    print(count)         
+
+if __name__ == "__main__":
+    main()
