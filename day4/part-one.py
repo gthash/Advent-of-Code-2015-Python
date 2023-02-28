b7ea4b1bf4d841fe5142d51c5b92b88e177696a4
gthash <74492250+gthash@users.noreply.github.com>
2023-02-28 14:27:30 -0300

    Create part-one.py

---
+++ day4/part-one.py
@@ -0,0 +1,13 @@
+import hashlib
+
+def main():
+    count = 0
+    result = hashlib.md5(b"ckczppom0")
+    while (result.hexdigest()[0:5] != "00000"):
+        count += 1
+        result = hashlib.md5(bytes("ckczppom" + str(count), "utf-8"))
+    print(count)
+
+if __name__ == "__main__":
+    main()
+
