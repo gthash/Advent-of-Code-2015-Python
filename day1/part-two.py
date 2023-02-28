e7955e948744642486f503c57efa8d00e7bce24d
gthash <74492250+gthash@users.noreply.github.com>
2023-02-28 11:38:33 -0300

    Add files via upload

---
+++ day1/part-two.py
@@ -0,0 +1,14 @@
+
+def main():
+    count, index = 0, 0
+    with open("input.txt") as file:
+        line = file.readline().strip()
+        for char in line:
+            count += 1 if char == "(" else -1
+            index += 1
+            if count == -1:
+                break
+    print(index)
+
+if __name__ == "__main__":
+    main()
