22b1140b4b14df8346baa1bfc50dd70328d620d2
gthash <74492250+gthash@users.noreply.github.com>
2023-03-01 16:42:25 -0300

    Update part-one.py

--- day8/part-one.py
+++ day8/part-one.py
@@ -1,7 +1,7 @@
 
 def main():
     count = 0
-    with open ("input.txt", 'r') as file:
+    with open ("input.txt", "r") as file:
         for line in file:
             count += (len(line.strip()) - len(eval(line)))
     print(count)
