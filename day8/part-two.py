eb9de462048a4342fb37e2a97d28de736aca93e6
gthash <74492250+gthash@users.noreply.github.com>
2023-03-01 16:38:02 -0300

    Add files via upload

---
+++ day8/part-two.py
@@ -0,0 +1,15 @@
+
+def main():
+    count = 0
+    with open ("input.txt", 'r') as file:
+        for line in file:
+            count += len(line.strip().replace('\\', r'\\') \
+                        .replace('"', '\\"')) + 2 - len(line.strip()) 
+    print(count)
+                
+if __name__ == "__main__":
+    main()
+
+
+
+
