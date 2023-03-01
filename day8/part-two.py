fd72d52746d34e512a215e4df85c64806609b272
gthash <74492250+gthash@users.noreply.github.com>
2023-03-01 16:41:52 -0300

    Update part-two.py

--- day8/part-two.py
+++ day8/part-two.py
@@ -1,13 +1,12 @@
 
 def main():
     count = 0
-    with open ("input.txt", 'r') as file:
+    with open ("input.txt", "r") as file:
         for line in file:
-            count += len(line.strip().replace('\\', r'\\') \
+            count += len(line.strip().replace("\\", r"\\") \
                         .replace('"', '\\"')) + 2 - len(line.strip()) 
     print(count)
                 
 if __name__ == "__main__":
     main()
-    
 
