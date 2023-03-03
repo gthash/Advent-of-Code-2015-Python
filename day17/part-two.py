781a10439bf30661ebb5d6d930ba5e280ce70836
gthash <74492250+gthash@users.noreply.github.com>
2023-03-03 20:11:49 -0300

    Update part-two.py

--- day17/part-two.py
+++ day17/part-two.py
@@ -3,18 +3,17 @@ import numpy as np
 
 def main():
     max_elem = int('11111111111111111111', 2)
-    possibilities, weights = [], 20 * [0]
-    for i in range(0, max_elem + 1):
-        possibilities.append(tuple((20 - len(bin(i)[2:])) * [0] + \
-                                    [int(x) for x in bin(i)[2:]]))
+    weights = 20 * [0]
     
     containers = np.array([33, 14, 18, 20, 45, 35, 16, 35, 1, 13, 18,
                            13, 50, 44, 48, 6, 24, 41, 30, 42 ])
     
-    for elements in possibilities:
-        result = np.multiply(elements, containers).sum()
+    for i in range(0, max_elem + 1):
+        binary = np.array((20 - len(bin(i)[2:])) * [0] + \
+                                    [int(x) for x in bin(i)[2:]])
+        result = sum(np.multiply(binary, containers))
         if result == 150:
-            weights[elements.count(1)] += 1
+            weights[np.count_nonzero(binary)] += 1
     
     print(next(x for x in weights if x != 0))
 
