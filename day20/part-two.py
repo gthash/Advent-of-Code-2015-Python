7713649ce7933f2dae78cb58c49d21e9f9649afd
gthash <74492250+gthash@users.noreply.github.com>
2023-03-03 23:57:19 -0300

    Add files via upload

---
+++ day20/part-two.py
@@ -0,0 +1,17 @@
+from sympy import divisors
+
+def main():
+    count, presents = 0, 0
+    while presents < 33100000:
+        count += 1; presents = 0
+        for divisor in divisors(count):
+            if divisor * 50 >= count:
+                presents += 11 * divisor
+
+    print(count)
+
+if __name__ == "__main__":
+    main()
+    
+
+
