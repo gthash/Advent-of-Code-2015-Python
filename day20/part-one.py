0563661b4d830af391e3c74b67bbad5c271b447c
gthash <74492250+gthash@users.noreply.github.com>
2023-03-03 23:56:19 -0300

    Create part-one.py

---
+++ day20/part-one.py
@@ -0,0 +1,13 @@
+from sympy import divisors
+
+def main():
+    count, presents = 0, 0
+    while presents < 33100000:
+        count += 1; presents = 0        
+        for divisor in divisors(count):
+            presents += 10 * divisor
+
+    print(count)
+
+if __name__ == "__main__":
+    main()
